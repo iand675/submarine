1276b6aca71f0f7f528fd2df997ff5cb6050c593
Ian Duncan <ian.duncan@logos.com>
2013-06-25 14:28:06 -0700

    Cut back on some parser redundancy

--- src/Network/URI/Template.hs
+++ src/Network/URI/Template.hs
@@ -20,31 +20,36 @@ data Modifier = Simple | Reserved | Fragment | Label | PathSegment | PathParamet
 range :: Char -> Char -> Parser Char
 range l r = satisfy (\c -> l <= c && c <= r)
 
+ranges :: [(Char, Char)] -> Parser Char
+ranges = choice . map (uncurry range)
+
 ucschar :: Parser Char
-ucschar
-	=   range '\xA0' '\xD7FF'
-	<|> range '\xF900' '\xFDCF'
-	<|> range '\xFDF0' '\xFFEF'
-	<|> range '\x10000' '\x1FFFD'
-	<|> range '\x20000' '\x2FFFD'
-	<|> range '\x30000' '\x3FFFD'
-	<|> range '\x40000' '\x4FFFD'
-	<|> range '\x50000' '\x5FFFD'
-	<|> range '\x60000' '\x6FFFD'
-	<|> range '\x70000' '\x7FFFD'
-	<|> range '\x80000' '\x8FFFD'
-	<|> range '\x90000' '\x9FFFD'
-	<|> range '\xA0000' '\xAFFFD'
-	<|> range '\xB0000' '\xBFFFD'
-	<|> range '\xC0000' '\xCFFFD'
-	<|> range '\xD0000' '\xDFFFD'
-	<|> range '\xE1000' '\xEFFFD'
+ucschar = ranges
+	[ ('\xA0', '\xD7FF')
+	, ('\xF900', '\xFDCF')
+	, ('\xFDF0', '\xFFEF')
+	, ('\x10000', '\x1FFFD')
+	, ('\x20000', '\x2FFFD')
+	, ('\x30000', '\x3FFFD')
+	, ('\x40000', '\x4FFFD')
+	, ('\x50000', '\x5FFFD')
+	, ('\x60000', '\x6FFFD')
+	, ('\x70000', '\x7FFFD')
+	, ('\x80000', '\x8FFFD')
+	, ('\x90000', '\x9FFFD')
+	, ('\xA0000', '\xAFFFD')
+	, ('\xB0000', '\xBFFFD')
+	, ('\xC0000', '\xCFFFD')
+	, ('\xD0000', '\xDFFFD')
+	, ('\xE1000', '\xEFFFD')
+	]
 
 iprivate :: Parser Char
-iprivate
-	=   range '\xE000' '\xF8FF'
-	<|> range '\xF0000' '\xFFFFD'
-	<|> range '\x100000' '\x10FFFD'
+iprivate = ranges
+	[ ('\xE000', '\xF8FF')
+	, ('\xF0000', '\xFFFFD')
+	, ('\x100000', '\x10FFFD')
+	]
 
 pctEncoded :: Parser String
 pctEncoded = do
@@ -54,11 +59,8 @@ pctEncoded = do
 	return [h, d1, d2]
 
 literalChar :: Parser Char
-literalChar
-	=   (choice $ map char ['\x21', '\x23', '\x24', '\x26', '\x3D', '\x5D', '\x5F', '\x7E'])
-	<|> range '\x28' '\x3B'
-	<|> range '\x3F' '\x5B'
-	<|> range '\x61' '\x7A'
+literalChar = (choice $ map char ['\x21', '\x23', '\x24', '\x26', '\x3D', '\x5D', '\x5F', '\x7E'])
+	<|> ranges [('\x28', '\x3B'), ('\x3F', '\x5B'), ('\x61', '\x7A')]
 	<|> ucschar
 	<|> iprivate
 
@@ -68,16 +70,18 @@ literal = (Literal . concat) <$> many1 ((pure <$> literalChar) <|> pctEncoded)
 variables :: Parser TemplateSegment
 variables = Embed <$> modifier <*> sepBy1 variable (spaces *> char ',' *> spaces)
 
+means :: Parser a -> b -> Parser b
+means p v = p *> pure v
+
+charMeans = means . char
+
 modifier :: Parser Modifier
-modifier
-	=   (char '+' *> pure Reserved)
-	<|> (char '#' *> pure Fragment)
-	<|> (char '.' *> pure Label)
-	<|> (char '/' *> pure PathSegment)
-	<|> (char ';' *> pure PathParameter)
-	<|> (char '?' *> pure Query)
-	<|> (char '&' *> pure QueryContinuation)
-	<|> pure Simple
+modifier = (choice $ map (uncurry charMeans) modifiers) <|> pure Simple
+	where modifiers =
+		[ ('+', Reserved), ('#', Fragment), ('.', Label)
+		, ('/', PathSegment), (';', PathParameter)
+		, ('?', Query), ('&', QueryContinuation)
+		]
 
 variable :: Parser Variable
 variable = concat <$> many1 ((pure <$> (alphaNum <|> char '_')) <|> pctEncoded)
