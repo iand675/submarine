824721121310bcfaec34b9be7a405094782edff0
Ian Duncan <iand675@gmail.com>
2013-06-27 23:37:09 -0700

    Grow ze APIs

--- src/Network/Intercom.hs
+++ src/Network/Intercom.hs
@@ -1,20 +1,32 @@
 module Network.Intercom where
 
-/users
-	-- get all
-	-- get single
-	-- create
-	-- update
-	-- delete
-/users/notes
-	-- create
-/users/impressions
-	-- create
-/users/message_threads
-	-- get
-	-- create
+listUsers
+listUsers = get [url| /users |]
+
+getUser :: Either UserId Email -> Intercom User
+getUser = get [url| /users{?user_id, email} |]
+
+createUser :: NewUser -> Intercom User
+createUser = post [url| /users |]
+
+deleteUser :: Either UserId Email -> Intercom User
+deleteUser = delete [url| /users{?user_id, email} |]
+
+createNote :: Either UserId Email -> Note -> Intercom Note
+createNote = post [url| /users/notes |]
+
+createImpression :: Either UserId Email -> Impression -> Intercom Int
+createImpression = post [url| /users/impressions |]
+
+listMessageThreads :: Either UserId Email -> Intercom [MessageThread]
+listMessageThreads = get [url| /users/message_threads |]
+
+getMessageThread = get [url| /users/message_threads |]
+
+createMessageThread :: 	-- create
+createMessageThread = post [url| /users/message_threads |]
 	-- reply
-/tags
+[url| /tags |]
 	-- get
 	-- create
 	-- update
--- src/Network/Mandrill.hs
+++ src/Network/Mandrill.hs
@@ -6,6 +6,8 @@ newtype Mandrill a = Mandrill
 
 runMandrill 
 
+data EmailStatus = Sent | Queued | Scheduled | Rejected | Invalid
+
 data WebHookEventType
 	= Send
 	| HardBounce
@@ -33,29 +35,171 @@ data WebHookEvent = WebHookEvent
 	, message :: MessageDetails
 	}
 
+data User = User
+	{ _userUsername :: Text
+	, _userCreatedAt :: UTCTime
+	, _userPublicId :: Text
+	, _userReputation :: Int
+	, _userHourlyQuota :: Int
+	, _userBacklog :: Int
+	, _userStats :: UserStats
+	}
+
+data UserStats = UserStats
+	{ _userstatsToday :: UserStat
+	, _userstatsLast7Days :: UserStat
+	, _userstatsLast30Days :: UserStat
+	, _userstatsLast60Days :: UserStat
+	, _userstatsLast90Days :: UserStat
+	, _userstatsAllTime :: UserStat
+	}
+
+data UserStat = UserStat
+	{ _userstatSent :: Int
+	, _userstatHardBounces :: Int
+	, _userstatSoftBounces :: Int
+	, _userstatRejects :: Int
+	, _userstatComplaints :: Int
+	, _userstatUnsubs :: Int
+	, _userstatOpens :: Int
+	, _userstatUniqueOpens :: Int
+	, _userstatClicks :: Int
+	, _userstatUniqueClicks :: Int
+	}
+
 -- /users/info.json
 getUserInfo :: Mandrill User
 getUserInfo = post [url| /users/info.json |]
 
--- /users/ping.json
-ping :: Mandrill Pong
-ping = post [url| /users/ping.json |]
+data Pong = Pong { ping :: Text }
 
 -- /users/ping2.json
-ping2 :: Mandrill Pong
-ping2 = post [url| /users/ping2.json |]
+ping :: Mandrill Pong
+ping = post [url| /users/ping2.json |]
+
+data Sender = Sender
+	{ _senderAddress :: Text
+	, _senderCreatedAt :: UTCTime
+	, _senderSent :: Int
+	, _senderHardBounces :: Int
+	, _senderSoftBounces :: Int
+	, _senderRejects :: Int
+	, _senderComplaints :: Int
+	, _senderUnsubs :: Int
+	, _senderOpens :: Int
+	, _senderClicks :: Int
+	, _senderUniqueOpens :: Int
+	, _senderUniqueClicks :: Int
+	}
 
 -- /users/senders.json
 listUserSenders :: Mandrill [Sender]
 listUserSenders = post [url| /users/senders.json |]
 
+data Destination = Destination
+	{ _destinationEmail :: Text
+	, _destinationName :: Text
+	}
+
+data MergeVariable = MergeVariable
+	{ _mergevariableName :: Text
+	, _mergevariableContent :: Text
+	}
+
+data MergeOverride = MergeOverride
+	{ _mergeoverrideRcpt :: Text
+	, _mergeoverrideVars :: [MergeVariable]
+	}
+
+data MetadataOverride = MetadataOverride
+	{ _metadataoverrideRcpt :: Text
+	, _metadataoverrideValues :: [Object]
+	}
+
+-- NEED CUSTOM JSON INSTANCE
+data Attachment = Attachment
+	{ _attachmentMimeType :: Text
+	, _attachmentName :: Text
+	, _attachmentContent :: ByteString
+	}
+
+-- NEED CUSTOM JSON INSTANCE
+data Image = Image
+	{ _imageMimeType :: Text
+	, _imageName :: Text
+	, _imageContent :: ByteString
+	}
+
+data Message f = Message
+	{ _messageHtml :: f Text
+	, _messageText :: f Text
+	, _messageSubject :: Text
+	, _messageFromEmail :: Text
+	, _messageFromName :: Text
+	, _messageTo :: [Destination]
+	, _messageHeaders :: HashMap Text Text
+	, _messageImportant :: Bool
+	, _messageTrackOpens :: Bool
+	, _messageTrackClicks :: Bool
+	, _messageAutoText :: Bool
+	, _messageAutoHtml :: Bool
+	, _messageInlineCss :: Bool
+	, _messageUrlStripQs :: Bool
+	, _messagePreserveRecipients :: Bool
+	, _messageBccAddress :: Text
+	, _messageTrackingDomain :: Text
+	, _messageSigningDomain :: Text
+	, _messageMerge :: Bool
+	, _messageGlobalMergeVars :: [MergeVariable]
+	, _messageMergeVars :: [MergeOverride]
+	, _messageTags :: [Text]
+	, _messageGoogleAnalyticsDomains :: [Text]
+	, _messageGoogleAnalyticsCampaign :: Text
+	, _messageMetadata :: Object
+	, _messageRecipientMetadata :: [MetadataOverride]
+	, _messageAttachments :: [Attachment]
+	, _messageImages :: [Image]
+	}
+
+data NewMessage = NewMessage
+	{ _newmessageMessage :: Message Identity
+	, _newmessageAsync :: Bool
+	, _newmessageIpPool :: Maybe Text
+	, _newmessageSendAt :: Maybe UTCTime
+	}
+
+data Recipient = Recipient
+	{ _recipientEmail :: Text
+	, _recipientStatus :: EmailStatus
+	, _recipientRejectReason :: Text
+	, _recipient_id :: Text
+	}
+
 -- /messages/send.json
 sendMessage :: NewMessage -> Mandrill [Recipient]
 sendMessage = post [url| /messages/send.json |]
 
+data NewTemplateMessage = NewTemplateMessage
+	{ _newtemplateMessage :: Message Maybe
+	, _newtemplateTemplateName :: Text
+	, _newtemplateTemplateContent :: [TemplateContent]
+	, _newtemplateAsync :: Bool
+	, _newtemplateIpPool :: Maybe Text
+	, _newtemplateSendAt :: Maybe UTCTime
+	}
+
 -- /messages/send-templates.json
 sendTemplateMessage :: NewTemplateMessage -> Mandrill [Recipient]
 sendTemplateMessage = post [url| /messages/send-templates.json |]
+
+data MessageSearch = MessageSearch
+	{ _messagesearchQuery :: Text
+	, _messagesearchDateFrom :: Maybe UTCTime
+	, _messagesearchDateTo :: Maybe UTCTime
+	, _messagesearchTags :: [Text]
+	, _messagesearchSenders :: [Text]
+	, _messagesearchLimit :: Maybe Int
+	}
 
 -- /messages/search.json
 searchSentMessages :: MessageSearch -> Mandrill [SentMessage]
