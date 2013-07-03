a94ad5170c0f1b2f60730f325b209a8951dc3b9d
Ian Duncan <iand675@gmail.com>
2013-07-02 22:40:55 -0700

    Aww yiss, stripe API

--- src/Network/Stripe.hs
+++ src/Network/Stripe.hs
@@ -1,8 +1,56 @@
 module Network.Stripe where
 
-newtype Stripe = Stripe { fromStripe :: ReaderT StripeConfig (ResourceT IO) a } deriving (Functor, Applicative, Monad)
 
--- Stripe API version 2013-02-13
+chargesUrl = [url| /v1/charges |]
+chargeUrl = [url| /v1/charges/{chargeId} |]
+couponsUrl = [url| /v1/coupons |]
+couponUrl = [url| /v1/coupons/{couponId} |]
+customersUrl [url| /v1/customers |]
+customerUrl = [url| /v1/customers/{customerId} |]
+customerSubscriptionUrl = [url| /v1/customers/{customerId}/subscription |]
+invoicesUrl = [url| /v1/invoices |]
+invoiceUrl = [url| /v1/invoices/{invoiceId} |]
+invoiceItemsUrl = [url| /v1/invoicesitems |]
+invoiceItemUrl = [url| /v1/invoicesitems/{invoiceItemId} |]
+plansUrl = [url| /v1/plans |]
+planUrl = [url| /v1/plans/{planId} |]
+eventsUrl = [url| /v1/events |]
+eventUrl = [url| /v1/events/{eventId} |]
+tokensUrl = [url| /v1/tokens |]
+tokenUrl = [url| /v1/tokens/{tokenId} |]
+
+data NewBankAccountToken = NewBankAccountToken
+	{ _newbankaccountCountry :: Text
+	, _newbankaccountRoutingNumber :: Text
+	, _newbankaccountAccountNumber :: Text
+	}
+
+data NewCharge = NewCharge
+	{ _newchargeAmount :: Cents
+	, _newchargeCurrency :: Text
+	, _newchargeIdentifier :: Either CustomerId CreditCard
+	, _newchargeDescription :: Maybe Text
+	, _newchargeCapture :: Maybe Bool
+	, _newchargeApplicationFee :: Maybe Cents
+	}
+
+data NewCardTokenCardInfo = NewCardTokenCardInfo
+	{ _newcardtokenNumber :: Text
+	, _newcardtokenExpirationMonth :: Int
+	, _newcardtokenExpirationYear :: Int
+	, _newcardtokenCvc :: Text
+	, _newcardtokenName :: Maybe Text
+	}
+
+data NewCardToken
+	= CardTokenCardInfo NewCardCardInfo
+	| CardTokenCustomer NewCardTokenCustomerInfo
+
+data Capture = Capture
+	{ _captureChargeId :: Text
+	, _captureAmount :: Maybe Cents
+	, _captureApplicationFee :: Maybe Bool
+	}
 
 data Charge = Charge
 	{ _chargeIdentifier :: Text
@@ -26,45 +74,65 @@ data Charge = Charge
 	, _chargeDispute :: Maybe Dispute
 	}
 
--- create charge
--- POST /v1/charges (NewCharge -> Charge)
-data NewCharge = NewCharge
-	{ _newchargeAmount :: Cents
-	, _newchargeCurrency :: Text
-	, _newchargeIdentifier :: Either CustomerId CreditCard
-	, _newchargeDescription :: Maybe Text
-	, _newchargeCapture :: Maybe Bool
-	, _newchargeApplicationFee :: Maybe Cents
-	}
-
--- get charge
--- GET /v1/charges/{CHARGE_ID}
-
--- refund charge
--- POST /v1/charges/{CHARGE_ID}/refund (Refund -> Charge)
 data Refund = Refund
 	{ _refundChargeId :: Text
 	, _refundAmount :: Maybe Cents
 	, _refundApplicationFee :: Maybe Bool
 	}
 
+newtype Stripe = Stripe { fromStripe :: ReaderT StripeConfig (ResourceT IO) a } deriving (Functor, Applicative, Monad)
+
+-- Stripe API version 2013-02-13
+
+
+-- create charge
+-- POST /v1/charges (NewCharge -> Charge)
+createCharge :: NewCharge -> Stripe Charge
+createCharge = post chargesUrl
+
+-- get charge
+-- GET /v1/charges/{CHARGE_ID}
+getCharge :: ChargeId -> Stripe Charge
+getCharge = get . chargeUrl
+
+-- refund charge
+-- POST /v1/charges/{CHARGE_ID}/refund (Refund -> Charge)
+refundCharge :: ChargeId -> Refund -> Stripe Charge
+refundCharge = post . (++ "/refund") . chargeUrl
+
 -- capture charge
 -- POST /v1/charges/{CHARGE_ID}/capture
-data Capture = Capture
-	{ _captureChargeId :: Text
-	, _captureAmount :: Maybe Cents
-	, _captureApplicationFee :: Maybe Bool
-	}
+captureCharge :: ChargeId -> Capture -> Stripe Charge
+captureCharge = post . (++ "/capture") . chargeUrl
 
 -- list all charges
+listCharges :: ListChargesQuery -> Stripe Charges
+listCharges q = get (chargesUrl ++ [url| {?q*} |])
 
 -- create customer
+createCustomer :: NewCustomer -> Stripe Customer
+createCustomer = post customersUrl
+
 -- get customer
+getCustomer :: CustomerId -> Stripe Customer
+getCustomer = get . customerUrl
+
 -- update customer
+updateCustomer :: CustomerId -> UpdatedCustomer -> Stripe Customer
+updateCustomer cId = post (customerUrl cId)
+
 -- delete customer
+deleteCustomer :: CustomerId -> Stripe DeletedResponse
+deleteCustomer = delete . customerUrl
+
 -- list all customers
+listCustomers :: ListCustomersQuery -> Stripe Customers
+listCustomers q = get (customersUrl ++ [url| {?q*} |])
 
 -- update subscription
+updateCustomerSubscription :: CustomerId -> UpdatedSubscription -> Stripe 
+updateCustomerSubscription 
+
 -- cancel subscription
 
 -- create plan
@@ -117,5 +185,8 @@ data Capture = Capture
 -- EVENT WEBHOOKS
 
 -- create card token
+
 -- create bank account token
+
+
 -- get token
