f54656e0631c9153708aec4306e7889468b78586
Ian Duncan <iand675@gmail.com>
2013-07-24 00:31:09 -0700

    Why not mandrill?

--- lib/mandrill/src/Mandrill.hs
+++ lib/mandrill/src/Mandrill.hs
@@ -1,10 +1,9 @@
 module Network.Mandrill where
+import API
 
 newtype Mandrill a = Mandrill
 	{ fromMandrill :: ReaderT MandrillConfig (ResourceT IO) a
 	} deriving (Functor, Applicative, Monad)
-
-runMandrill 
 
 data EmailStatus = Sent | Queued | Scheduled | Rejected | Invalid
 
@@ -69,13 +68,13 @@ data UserStat = UserStat
 
 -- /users/info.json
 getUserInfo :: Mandrill User
-getUserInfo = post [url| /users/info.json |]
+getUserInfo = post "/users/info.json"
 
 data Pong = Pong { ping :: Text }
 
 -- /users/ping2.json
 ping :: Mandrill Pong
-ping = post [url| /users/ping2.json |]
+ping = post "/users/ping2.json"
 
 data Sender = Sender
 	{ _senderAddress :: Text
@@ -94,7 +93,7 @@ data Sender = Sender
 
 -- /users/senders.json
 listUserSenders :: Mandrill [Sender]
-listUserSenders = post [url| /users/senders.json |]
+listUserSenders = post "/users/senders.json"
 
 data Destination = Destination
 	{ _destinationEmail :: Text
@@ -177,7 +176,7 @@ data Recipient = Recipient
 
 -- /messages/send.json
 sendMessage :: NewMessage -> Mandrill [Recipient]
-sendMessage = post [url| /messages/send.json |]
+sendMessage = post "/messages/send.json"
 
 data NewTemplateMessage = NewTemplateMessage
 	{ _newtemplateMessage :: Message Maybe
@@ -190,7 +189,7 @@ data NewTemplateMessage = NewTemplateMessage
 
 -- /messages/send-templates.json
 sendTemplateMessage :: NewTemplateMessage -> Mandrill [Recipient]
-sendTemplateMessage = post [url| /messages/send-templates.json |]
+sendTemplateMessage = post "/messages/send-templates.json"
 
 data MessageSearch = MessageSearch
 	{ _messagesearchQuery :: Text
@@ -203,180 +202,180 @@ data MessageSearch = MessageSearch
 
 -- /messages/search.json
 searchSentMessages :: MessageSearch -> Mandrill [SentMessage]
-searchSentMessages = post [url| /messages/search.json |]
+searchSentMessages = post "/messages/search.json"
 
 -- /messages/parse.json
 parseMessage :: Text -> Mandrill Message
-parseMessage = post [url| /messages/parse.json |]
+parseMessage = post "/messages/parse.json"
 
 -- /messages/send-raw.json
 sendRawMessage :: RawMessage -> Mandrill [Recipient]
-sendRawMessage = post [url| /messages/send-raw.json |]
+sendRawMessage = post "/messages/send-raw.json"
 
 -- /messages/list-scheduled.json
 listScheduledMessages :: ScheduledMessageSearchOptions -> Mandrill [ScheduledMessage]
-listScheduledMessages = post [url| /messages/list-scheduled.json |]
+listScheduledMessages = post "/messages/list-scheduled.json"
 
 -- /messages/cancel-scheduled.json
 cancelScheduledMessage :: MessageCancellationOptions -> Mandrill CancelledMessage
-cancelScheduledMessage = post [url| /messages/cancel-scheduled.json |]
+cancelScheduledMessage = post "/messages/cancel-scheduled.json"
 
 -- /messages/reschedule.json
 rescheduleMessage :: RescheduleMessageOptions -> Mandrill RescheduledMessage
-rescheduleMessage = post [url| /messages/reschedule.json |]
+rescheduleMessage = post "/messages/reschedule.json"
 
 -- /tags/list.json
 listTags :: Mandrill [Tag]
-listTags = post [url| /tags/list.json |]
+listTags = post "/tags/list.json"
 
 -- /tags/delete.json
 deleteTag :: TagName -> Mandrill Tag
-deleteTag = post [url| /tags/delete.json |]
+deleteTag = post "/tags/delete.json"
 
 -- /tags/info.json
 getTagInfo :: TagName -> Mandrill TagInfo
-getTagInfo = post [url| /tags/info.json |]
+getTagInfo = post "/tags/info.json"
 
 -- /tags/time-series.json
 getTagTimeSeries :: TagName -> Mandrill [TagTimeSeriesInfo]
-getTagTimeSeries = post [url| /tags/time-series.json |]
+getTagTimeSeries = post "/tags/time-series.json"
 
 -- /tags/all-time-series.json
 getAllTagTimeSeries :: Mandrill [TagTimeSeriesInfo]
-getAllTagTimeSeries = post [url| /tags/all-time-series.json |]
+getAllTagTimeSeries = post "/tags/all-time-series.json"
 
 -- /rejects/add.json
 blacklistEmail :: EmailAddress -> Mandrill [BlacklistInfo]
-blacklistEmail = post [url| /rejects/add.json |]
+blacklistEmail = post "/rejects/add.json"
 
 -- /rejects/list.json
 listBlacklistedEmails :: BlacklistListFilterOptions -> Mandrill [BlacklistedEmail]
-listBlacklistedEmails = post [url| /rejects/list.json |]
+listBlacklistedEmails = post "/rejects/list.json"
 
 -- /rejects/delete.json
 deleteBlacklistedEmail :: EmailAddress -> Mandrill [BlacklistDeletionInfo]
-deleteBlacklistedEmail = post [url| /rejects/delete.json |]
+deleteBlacklistedEmail = post "/rejects/delete.json"
 
 -- /whitelist/add.json
 whitelistEmail :: EmailAddress -> Mandrill [WhitelistInfo]
-whitelistEmail = post [url| /whitelist/add.json |]
+whitelistEmail = post "/whitelist/add.json"
 
 -- /whitelist/list.json
 listWhitelistedEmails :: WhitelistListFilterOptions -> Mandrill [WhitelistedEmail]
-listWhitelistedEmails = post [url| /whitelist/list.json |]
+listWhitelistedEmails = post "/whitelist/list.json"
 
 -- /whitelist/delete.json
 deleteWhitelistedEmail :: EmailAddress -> Mandrill [WhitelistDeletionInfo]
-deleteWhitelistedEmail = post [url| /whitelist/delete.json |]
+deleteWhitelistedEmail = post "/whitelist/delete.json"
 
 -- /senders/list.json
 listSenders :: Mandrill [Sender]
-listSenders = post [url| /senders/list.json |]
+listSenders = post "/senders/list.json"
 
 -- /senders/domains.json
 listSenderDomains :: Mandrill [Domain]
-listSenderDomains = post [url| /senders/domains.json |]
+listSenderDomains = post "/senders/domains.json"
 
 -- /senders/info.json
 getSenderInfo :: EmailAddress -> Mandrill [SenderInfo]
-getSenderInfo = post [url| /senders/info.json |]
+getSenderInfo = post "/senders/info.json"
 
 -- /senders/time-series.json
 getSenderTimeSeries :: EmailAddress -> Mandrill [SenderTimeSeries]
-getSenderTimeSeries = post [url| /senders/time-series.json |]
+getSenderTimeSeries = post "/senders/time-series.json"
 
 -- /urls/list.json
 listUrls :: Mandrill [UrlInfo]
-listUrls = post [url| /urls/list.json |]
+listUrls = post "/urls/list.json"
 
 -- /urls/search.json
 searchUrls :: UrlSearchQuery -> Mandrill [UrlInfo]
-searchUrls = post [url| /urls/search.json |]
+searchUrls = post "/urls/search.json"
 
 -- /urls/time-series.json
 getUrlTimeSeries :: Url -> Mandrill [UrlInfo]
-getUrlTimeSeries = post [url| /urls/time-series.json |]
+getUrlTimeSeries = post "/urls/time-series.json"
 
 -- /templates/add.json
 addTemplate :: NewTemplate -> Mandrill Template
-addTemplate = post [url| /templates/add.json |]
+addTemplate = post "/templates/add.json"
 
 -- /templates/info.json
 getTemplateInfo :: TemplateName -> Mandrill Template
-getTemplateInfo = post [url| /templates/info.json |]
+getTemplateInfo = post "/templates/info.json"
 
 -- /templates/update.json
 updateTemplate :: NewTemplate -> Mandrill Template
-updateTemplate = post [url| /templates/update.json |]
+updateTemplate = post "/templates/update.json"
 
 -- /templates/publish.json
 publishTemplate :: TemplateName -> Mandrill Template
-publishTemplate = post [url| /templates/publish.json |]
+publishTemplate = post "/templates/publish.json"
 
 -- /templates/delete.json
 deleteTemplate :: TemplateName -> Mandrill Template
-deleteTemplate = post [url| /templates/delete.json |]
+deleteTemplate = post "/templates/delete.json"
 
 -- /templates/list.json
 listTemplates :: Mandrill [Template]
-listTemplates = post [url| /templates/list.json |]
+listTemplates = post "/templates/list.json"
 
 -- /templates/time-series.json
 getTemplateTimeSeries :: TemplateName -> Mandrill Template
-getTemplateTimeSeries = post [url| /templates/time-series.json |]
+getTemplateTimeSeries = post "/templates/time-series.json"
 
 -- /templates/render.json
 renderTemplate :: TemplateContent -> Mandrill RenderedTemplate
-renderTemplate = post [url| /templates/render.json |]
+renderTemplate = post "/templates/render.json"
 
 -- /webhooks/list.json
 listWebhooks :: Mandrill [WebHook]
-listWebhooks = post [url| /webhooks/list.json |]
+listWebhooks = post "/webhooks/list.json"
 
 -- /webhooks/add.json
 addWebhook :: NewWebhook -> Mandrill WebHook
-addWebhook = post [url| /webhooks/add.json |]
+addWebhook = post "/webhooks/add.json"
 
 -- /webhooks/info.json
 getWebhookInfo :: WebHookId -> Mandrill WebHook
-getWebhookInfo = post [url| /webhooks/info.json |]
+getWebhookInfo = post "/webhooks/info.json"
 
 -- /webhooks/update.json
 updateWebhook :: NewWebhook -> Mandrill WebHook
-updateWebhook = post [url| /webhooks/update.json |]
+updateWebhook = post "/webhooks/update.json"
 
 -- /webhooks/delete.json
 deleteWebhook :: WebHookId -> Mandrill WebHook
-deleteWebhook = post [url| /webhooks/delete.json |]
+deleteWebhook = post "/webhooks/delete.json"
 
 -- /inbound/domains.json
 listInboundDomains :: Mandrill [InboundDomain]
-listInboundDomains = post [url| /inbound/domains.json |]
+listInboundDomains = post "/inbound/domains.json"
 
 -- /inbound/routes.json
 listInboundRoutes :: DomainName -> Mandrill [InboundRoute]
-listInboundRoutes = post [url| /inbound/routes.json |]
+listInboundRoutes = post "/inbound/routes.json"
 
 -- /inbound/send-raw.json
 sendRawDocument :: RawMessageInfo -> Mandrill SentRawDocument
-sendRawDocument = post [url| /inbound/send-raw.json |]
+sendRawDocument = post "/inbound/send-raw.json"
 
 -- /exports/info.json
 getExportInfo :: ExportId -> Mandrill Export
-getExportInfo = post [url| /exports/info.json |]
+getExportInfo = post "/exports/info.json"
 
 -- /exports/list.json
 listExports :: Mandrill [Export]
-listExports = post [url| /exports/list.json |]
+listExports = post "/exports/list.json"
 
 -- /exports/rejects.json
 exportBlacklist :: Email -> Mandrill Export
-exportBlacklist = post [url| /exports/rejects.json |]
+exportBlacklist = post "/exports/rejects.json"
 
 -- /exports/whitelist.json
 exportWhitelist :: Email -> Mandrill Export
-exportWhitelist = post [url| /exports/whitelist.json |]
+exportWhitelist = post "/exports/whitelist.json"
 
 -- /exports/activity.json
 exportActivity :: ActivityExportOptions -> Mandrill Export
-exportActivity = post [url| /exports/activity.json |]
+exportActivity = post "/exports/activity.json"
