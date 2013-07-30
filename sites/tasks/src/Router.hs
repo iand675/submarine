c8d20353f9730b3ec165fc47b303571b7ed57edc
Ian Duncan <ian.duncan@logos.com>
2013-07-30 16:36:44 -0700

    Some restructuring stuff

---
+++ sites/tasks/src/Router.hs
@@ -0,0 +1,28 @@
+module Router where
+
+{-
+	get users
+	create user
+	get user
+	delete user
+	patch user
+	get user projects
+	log in
+
+	get groups
+	get group
+	create group
+	patch group
+	get group projects
+
+	create list
+	patch list
+	delete list
+
+	get list tasks
+	create task
+	patch task
+	delete task
+	get tasks
+	get task
+-}
