5a3dbd28808972b247bf2f9f851b6aaddc1d33d6
Ian Duncan <iand675@gmail.com>
2013-08-02 00:33:51 -0700

    Not too much useful in this one.

--- sites/tasks-api/src/Backend.hs
+++ sites/tasks-api/src/Backend.hs
@@ -9,7 +9,9 @@ newtype Identity a = Identity { fromIdentity :: a }
 -- stubs
 data User
 data List
-data Project
+data Category
+data Team
+data Organization
 
 data Task f = Task
 	{ taskName :: f String
@@ -27,11 +29,31 @@ newtype Id a = Id String
 
 data TaskBackend m = TaskBackend
 	{ createTask :: NewTask -> m (Id Task, FullTask)
-	, readTask   :: Id Task -> m (Maybe FullTask)
+	, getTask    :: Id Task -> m (Maybe FullTask)
 	, updateTask :: Id Task -> TaskPatch -> m (Maybe FullTask)
 	, deleteTask :: Id Task -> m (Maybe FullTask)
 	, listTasks  :: TaskQuery -> m [(Id Task, FullTask)]
 	}
+
+data UserBackend m = UserBackend
+  { createUser :: NewUser -> m (Id User, FullUser)
+  , getUser    :: Id User -> m (Maybe FullUser)
+  , updateUser :: Id User -> UserPatch -> m (Maybe FullUser)
+  , deleteUser :: Id User -> m (Maybe FullUser)
+  , listUsers  :: UserQuery -> m [(Id User, FullUser)]
+  }
+
+data ListBackend m = ListBackend
+  { createList :: NewList -> m (Id List, FullList)
+  , getList    :: Id List -> m (Maybe List)
+  , updateList :: Id List -> ListPatch -> m (Maybe List)
+  , deleteList :: Id List -> m (Maybe List)
+  , listLists  :: ListQuery -> m [(Id List, FullList)]
+  }
+
+-- data CategoryBackend m
+-- data TeamBackend m
+-- data OrganizationBackend m
 
 data TaskQuery
 	= All
@@ -54,3 +76,15 @@ inMemoryBackend = do
 		, deleteTask = undefined
 		, listTasks  = undefined
 		}
+
+createTaskImpl nt = do
+  newTask <- initializeDefaults nt
+  newTaskId <- postgres $ do
+    insert into Tasks newTask
+  redis $ do
+    setEx (taskNamespace newTaskId) newTask
+  amqp $ publish tasksQueue 
+
+
+
+
