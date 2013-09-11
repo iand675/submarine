a9a361c03ff01ecdbe5d618c2efa876c220dd883
Ian Duncan <ian.duncan@logos.com>
2013-09-10 17:00:39 -0700

    Build some more homegrown cabal helper code.

--- Setup.hs
+++ Setup.hs
@@ -1,22 +1,47 @@
 module Main where
+import Control.Applicative
+import Data.List
 import System.Directory
+import System.Environment
 import System.FilePath
 import System.Cmd
 
+main = do
+	args <- getArgs
+	--case args of
+		--["init"] -> initializeSandboxing
+		--["install-dependencies"] -> installDependencies
+		--_ -> putStrLn "Doing nothing"
+	installDependencies
+
+getDirectoryListing dir = do
+	dirs <- drop 2 <$> getDirectoryContents dir
+	return $! ((dir </>) <$> dirs)
+
+installDependencies = do
+	allProjects <- concat <$> mapM getDirectoryListing ["lib", "sites", "workers"]
+	let installProjectDependenciesCmd p = "cabal install -j --only-dependencies " ++ p
+	let installProjectDependencies p = putStrLn (installProjectDependenciesCmd p) >> system (installProjectDependenciesCmd p)
+	mapM_ installProjectDependencies allProjects
+
 initializeSandboxing = do
-	currentDir <- getCurrentDirectory
-	let executableDirectories = ["sites/", "workers/"]
-	let projectDirectories = "lib/" : executableDirectories
 	system "cabal sandbox init"
-	projectsByDirectory <- mapM getDirectoryContents executableDirectories
-	libraries <- fmap (drop 2) $ getDirectoryContents "lib/"
-	let projectList = concat $ zipWith (\dir -> map (dir ++)) projectDirectories (libraries : map (drop 2) projectsByDirectory)
+	currentDir <- getCurrentDirectory
+	let projectDirectories = ["lib", "sites", "workers"]
+	projects <- concat <$> mapM getDirectoryListing projectDirectories
 	let initializeSandboxForPath p = do
 		setCurrentDirectory (currentDir </> p)
 		system ("cabal sandbox init --sandbox " ++ currentDir ++ "/.cabal-sandbox")
 		setCurrentDirectory currentDir
-	let addLibrarySource l = do
-		putStrLn ("Adding source lib/" ++ l)
-		system ("cabal sandbox add-source lib/" ++ l)
-	mapM_ initializeSandboxForPath projectList
+	mapM_ initializeSandboxForPath projects
+	
+
+addLibrarySources = do
+	libraries <- getDirectoryListing "lib"
 	mapM_ addLibrarySource libraries
+
+addLibrarySource l = do
+	putStrLn ("Adding source " ++ l)
+	system ("cabal sandbox add-source " ++ l)
+
+	
